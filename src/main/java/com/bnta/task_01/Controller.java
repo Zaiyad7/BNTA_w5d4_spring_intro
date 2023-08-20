13603fec49431c4dc54a89c8f0b54169e6a4b408
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-08-20 15:13:47 +0100

    added request param annotation to take timeOfDay from url

--- src/main/java/com/bnta/task_01/Controller.java
+++ src/main/java/com/bnta/task_01/Controller.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +15,10 @@ public class Controller {
 
 
     @GetMapping
-    public ResponseEntity<String> getGreeting(){
+    public ResponseEntity<String> getGreeting(@RequestParam(defaultValue = "Day") String timeOfDay){
         this.greeting = new Greeting("Colin", "Daytime");
-        return new ResponseEntity<>("Good afternoon " + greeting.getName() + "!", HttpStatus.OK);
+        return new ResponseEntity<>("Good " + timeOfDay + " " + greeting.getName() + "!",
+                HttpStatus.OK);
 
     }
 }
