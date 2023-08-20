0867effbc86d0611383d00246c709e75d7cfc8ef
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-08-20 15:16:58 +0100

    added CelebrationDTO

---
+++ src/main/java/com/bnta/task_01/Celebration.java
@@ -0,0 +1,22 @@
+package com.bnta.task_01;
+
+public class Celebration {
+
+    private String message;
+
+    public Celebration(String message) {
+        this.message = message;
+    }
+
+    public Celebration() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
